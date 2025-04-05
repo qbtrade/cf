0a10ad7cb2b451c86ef26be59cd938136308e116
tyz <tyz@1token.trade>
2025-04-05 13:11:58 +0800

    ♻️ refactor(init.py)：重构日志初始化以支持全局日志记录器

--- src/cf/__init__.py
+++ src/cf/__init__.py
@@ -5,20 +5,30 @@ __email__ = "tyz@1token.trade"
 __version__ = "0.1.0"
 import sys
 
-from loguru import logger
+from loguru import logger as global_logger
 
 
-def init_loguru():
+logger = global_logger.bind(name="cf")
+
+logger.add(
+    sink=sys.stderr,
+    format="<green>{time:HH:mm:ss.SSS}</green> | <level>{level:8}</level> | {file.name}:{line} <cyan>{message}</cyan>",
+    level="DEBUG",
+)
+
+
+def init():
+    init_loguru_global()
+
+
+def init_loguru_global():
 
     # 12:56:05.284 | DEBUG    | download_image.py:93
 
-    logger.remove()  # 移除默认的 handler
+    global_logger.remove()  # 移除默认的 handler
 
-    logger.add(
+    global_logger.add(
         sink=sys.stderr,
         format="<green>{time:HH:mm:ss.SSS}</green> | <level>{level:8}</level> | {file.name}:{line} <cyan>{message}</cyan>",
         level="DEBUG",
     )
-
-
-__all__ = ["logger"]
